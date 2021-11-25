ed03124bdb6d7ec8b20ea791df4186bed9bf138d
Andrei <andreidombi99@gmail.com>
2021-11-25 21:23:09 +0200

    added maven connector

--- src/main/java/Validate.java
+++ src/main/java/Validate.java
@@ -7,7 +7,7 @@ public class Validate {
         try {
 
             //loading drivers for mysql
-            //Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
 
             //creating connection with the database
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/users_db","root","andreidombi");
